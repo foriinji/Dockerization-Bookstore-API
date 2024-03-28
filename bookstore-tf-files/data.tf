30b8b3b3cdbbf096b6b3a3fbb1528052827f4b28
Mehmet Denizli <154231505+foriinji@users.noreply.github.com>
2024-03-28 22:17:27 +0300

    Update data.tf

--- bookstore-tf-files/data.tf
+++ bookstore-tf-files/data.tf
@@ -1,19 +1,1 @@
-data "aws_ami" "linux2" {
-  most_recent = true
-  owners      = ["amazon"]
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-kernel-*"]
-  }
-}
